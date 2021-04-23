b2963d45f7f1c2210d96939ca48dbe133dfa3492
Wali <wali.boucher@gmail.com>
2021-04-23 09:19:11 +0200

    Boutons

--- src/gui/Fenetre.java
+++ src/gui/Fenetre.java
@@ -25,8 +25,8 @@ class Fenetre extends JFrame implements ActionListener { // Template JFrame, ext
         
         //Creer une fenetre d'affichage dans l'Interface (voir setBounds)
         
-        // create a new buttons
-        //Voir comment bien placer les boutons
+        // create new buttons
+   
         JButton b = new JButton("0");
         JButton b1 = new JButton("1");
         JButton b2 = new JButton("2");
@@ -40,31 +40,61 @@ class Fenetre extends JFrame implements ActionListener { // Template JFrame, ext
         JButton o1 = new JButton("+");
 
         
-        // create a panel to add buttons
-        JPanel p = new JPanel();
         
-        // add buttons and textfield to panel
-        p.add(b);
-        p.add(b1);
-        p.add(b2);
-        p.add(b3);
-        p.add(b4);
-        p.add(b5);
-        p.add(b6);
-        p.add(b7);
-        p.add(b8);
-        p.add(b9);
-        p.add(o1);
-
-        p.add(l);
+        JPanel ligne0 = new JPanel();
+        ligne0.setLayout(new BoxLayout (ligne0, BoxLayout.LINE_AXIS));
+        ligne0.add(l);
+        // create a panel to add buttons
+        JPanel ligne1 = new JPanel();
+        
+        // add buttons and textfield on line 1
+       ligne1.setLayout(new BoxLayout (ligne1, BoxLayout.LINE_AXIS));
+        ligne1.add(b1);
+        ligne1.add(b2);
+        ligne1.add(b3);
+        
+        //Idem ligne 2
+        JPanel ligne2 = new JPanel();
+        ligne2.setLayout(new BoxLayout (ligne2, BoxLayout.LINE_AXIS));
+        
+        ligne2.add(b4);
+        ligne2.add(b5);
+        ligne2.add(b6);
+        
+        //Ligne 3
+        JPanel ligne3 = new JPanel();
+        
+        ligne3.setLayout(new BoxLayout (ligne3, BoxLayout.LINE_AXIS));
+        ligne3.add(b7);
+        ligne3.add(b8);
+        ligne3.add(b9);
+        
+        //Ligne4
+        JPanel ligne4 = new JPanel();
+        ligne4.setLayout(new BoxLayout (ligne4, BoxLayout.LINE_AXIS));
+        ligne4.add(o1);
+        ligne4.add(b);
+       
+        
+        //On crée 3 colonnes
+        
+        JPanel colonnes = new JPanel();
+        colonnes.setLayout(new BoxLayout(colonnes, BoxLayout.PAGE_AXIS));
+        colonnes.add(ligne0);
+        colonnes.add(ligne1);
+        colonnes.add(ligne2);
+        colonnes.add(ligne3);
+        colonnes.add(ligne4);
+        
+        maFenetre.getContentPane().add(colonnes);
         
         
         
         // setbackground of panel
-        p.setBackground(Color.red);
+      //  p.setBackground(Color.red);
         
         // add panel to frame
-        maFenetre.add(p);
+      //  maFenetre.add(p);
         
         // set the size of frame
         maFenetre.setSize(300, 300);
